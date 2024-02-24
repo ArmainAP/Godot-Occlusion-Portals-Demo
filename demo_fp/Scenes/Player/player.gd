a052c665e561030c6d8876a86a169922546ff3e7
ArmainAP <adrian.popoviciu@katcodelabs.com>
2024-02-24 17:45:40 +0200

    +Added basic block portal shader -Removed unused content

--- demo_fp/Scenes/Player/player.gd
+++ demo_fp/Scenes/Player/player.gd
@@ -2,12 +2,8 @@ extends CharacterBody3D
 
 @onready var gunRay := $Head/Camera3d/RayCast3d as RayCast3D
 @onready var Cam := $Head/Camera3d as Camera3D
-@export var _bullet_scene : PackedScene
 var mouseSensibility := 1200
-var mouse_relative_x := 0
-var mouse_relative_y := 0
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -20,13 +16,6 @@ func _physics_process(delta : float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	# Handle Shooting
-	if Input.is_action_just_pressed("Shoot"):
-		shoot()
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir := Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -45,16 +34,3 @@ func _input(event : InputEvent) -> void:
 		rotation.y -= mouse_event.relative.x / mouseSensibility
 		Cam.rotation.x -= mouse_event.relative.y / mouseSensibility
 		Cam.rotation.x = clamp(Cam.rotation.x, deg_to_rad(-90), deg_to_rad(90) )
-		mouse_relative_x = clamp(mouse_event.relative.x, -50, 50)
-		mouse_relative_y = clamp(mouse_event.relative.y, -50, 10)
-
-func shoot() -> void:
-	if not gunRay.is_colliding():
-		return
-	var bulletInst := _bullet_scene.instantiate() as Node3D
-	bulletInst.set_as_top_level(true)
-	get_parent().add_child(bulletInst)
-	bulletInst.global_transform.origin = gunRay.get_collision_point() as Vector3
-	bulletInst.look_at((gunRay.get_collision_point()+gunRay.get_collision_normal()),Vector3.BACK)
-	print(gunRay.get_collision_point())
-	print(gunRay.get_collision_point()+gunRay.get_collision_normal())
